31c6f2444be50ad8355ce7abdb8433596d137766
Yurii Drutsul <druculurij@gmail.com>
2025-11-03 14:15:26 +0200

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const populationValues = [...document.querySelectorAll('.population')].map(
+  (el) => Number(el.textContent.split(',').join('')),
+);
+
+const sum = populationValues.reduce((acc, val) => acc + val, 0);
+const average = Math.round(sum / populationValues.length);
+
+document.querySelector('.total-population').textContent =
+  sum.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-US');
